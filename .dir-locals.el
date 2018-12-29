ac9507207fcdb257b244c4b7b90dbfa871f5576c
Goichi Hirakawa <gooichi@gyazsquare.com>
2018-12-29 21:08:57 +0900

    refs #21 Fix .dir-locals.el for emacs 26

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,17 +1,13 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil
-  . ((eval . (setenv "DEVELOPER_DIR"
-                     "/Applications/Xcode.app/Contents/Developer"))
-     (fill-column . 80)
-     (whitespace-style . (face lines indentation:space))
-     (eval . (set (make-local-variable 'project-dir)
-                  (file-name-directory
-                   (let ((d (dir-locals-find-file ".")))
-                     (if (stringp d) d (car d))))))
-     (eval . (set (make-local-variable 'srcroot)
-                  (expand-file-name "GSLCSVSerialization" project-dir)))))
+(("GSLCSVSerializationTests"
+  . ((objc-mode
+      . ((eval . (add-to-list
+                  'flycheck-objc-clang-framework-paths
+                  (expand-file-name
+                   "Platforms/iPhoneOS.platform/Developer/Library/Frameworks"
+                   (getenv "DEVELOPER_DIR"))))))))
  (objc-mode
   . ((flycheck-objc-clang-xcrun-sdk . "iphoneos")
      (flycheck-objc-clang-arc . t)
@@ -19,10 +15,14 @@
      (flycheck-objc-clang-archs . ("arm64" "armv7"))
      (flycheck-objc-clang-ios-version-min . "8.0")
      (eval . (set 'flycheck-objc-clang-include-paths (list srcroot)))))
- ("GSLCSVSerializationTests"
-  . ((objc-mode
-      . ((eval . (add-to-list
-                  'flycheck-objc-clang-framework-paths
-                  (expand-file-name
-                   "Platforms/iPhoneOS.platform/Developer/Library/Frameworks"
-                   (getenv "DEVELOPER_DIR")))))))))
+ (nil
+  . ((eval . (setenv "DEVELOPER_DIR"
+                     "/Applications/Xcode.app/Contents/Developer"))
+     (fill-column . 80)
+     (whitespace-style . (face lines indentation:space))
+     (eval . (set (make-local-variable 'project-dir)
+                  (file-name-directory
+                   (let ((d (or (dir-locals-find-file ".") default-directory)))
+                     (if (stringp d) d (car d))))))
+     (eval . (set (make-local-variable 'srcroot)
+                  (expand-file-name "GSLCSVSerialization" project-dir))))))
